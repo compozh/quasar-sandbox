7321c55f68c79ec3fed327360b7777b3664e78f1
Igor Pevchev <pevchev@it-enterprise.com>
2020-02-14 14:28:46 +0200

    form data added

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -21,12 +21,43 @@
         q-toggle(v-model="accept" label="I accept the license and terms")
 
         div
-          q-btn(label="Submit" type="submit" color="primary")
+          q-btn(:label="$q.lang.label.create" type="submit" color="primary")
           q-btn(label="Reset" type="reset" color="primary" flat class="q-ml-sm")
 </template>
 
 <script>
 export default {
-  name: 'PageIndex'
+  data () {
+    return {
+      name: null,
+      age: null,
+
+      accept: false
+    }
+  },
+  methods: {
+    onSubmit () {
+      if (this.accept !== true) {
+        this.$q.notify({
+          color: 'red-5',
+          textColor: 'white',
+          icon: 'warning',
+          message: 'You need to accept the license and terms first'
+        })
+      } else {
+        this.$q.notify({
+          color: 'green-4',
+          textColor: 'white',
+          icon: 'cloud_done',
+          message: 'Submitted'
+        })
+      }
+    },
+    onReset () {
+      this.name = null
+      this.age = null
+      this.accept = false
+    }
+  }
 }
 </script>
