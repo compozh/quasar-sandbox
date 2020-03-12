7ccebb7d4b7b3fedb5cc63036fc05dcb79a8fd22
Igor Pevchev <pevchev@it-enterprise.com>
2020-03-12 19:13:16 +0200

    update main layout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -8,15 +8,7 @@
 
         q-toolbar-title Quasar App
 
-        q-select(v-model="lang"
-                 :options="langOptions"
-                 :label="$t('language')"
-                 dense
-                 borderless
-                 emit-value
-                 map-options
-                 options-dense
-                 style="min-width: 150px")
+        lang-switcher
 
         div Quasar v {{ $q.version }}
 
@@ -33,20 +25,16 @@
 </template>
 
 <script>
-import languages from 'quasar/lang/index.json'
+import LangSwitcher from 'components/LangSwitcher'
 import EssentialLink from 'components/EssentialLink'
-import { format } from 'quasar'
-
-const appLanguages = languages.filter(lang =>
-  [ 'ru', 'uk', 'en-us' ].includes(lang.isoName))
 
 export default {
   name: 'MainLayout',
   components: {
+    LangSwitcher,
     EssentialLink
   },
   data: () => ({
-    langOptions: [],
     leftDrawerOpen: false,
     essentialLinks: [
       {
@@ -56,32 +44,6 @@ export default {
         link: 'https://quasar.dev'
       }
     ]
-  }),
-  computed: {
-    lang: {
-      get () {
-        return this.$i18n.locale
-      },
-      set (lang) {
-        this.$i18n.locale = lang
-      }
-    }
-  },
-  watch: {
-    lang (lang) {
-      import(/* webpackInclude: /(ru|uk|en-us)\.js$/ */
-      'quasar/lang/' + lang)
-        .then(lang => this.$q.lang.set(lang.default))
-    }
-  },
-  created () {
-    const { capitalize } = format
-    const label = name => name.slice(0, 2).toUpperCase()
-    this.langOptions = appLanguages.map(lang => ({
-      label: capitalize(lang.nativeName),
-      icon: label(lang.isoName),
-      value: lang.isoName
-    }))
-  }
+  })
 }
 </script>
