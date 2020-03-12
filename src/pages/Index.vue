0c159bfcbee770d2b282edf3523d27c19502ebbd
Igor Pevchev <pevchev@it-enterprise.com>
2020-03-12 19:13:33 +0200

    update content

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,98 +1,81 @@
 <template lang="pug">
   q-page.flex.flex-center
-    .q-pa-md(style="max-width: 400px")
+    .q-pa-md
 
       q-form(@submit="onSubmit"
-             @reset="onReset").q-gutter-md
-        div {{ date }}
+             @reset="onReset")
 
-        q-input(filled v-model="date" :mask="$t('dateInputMask')" :rules="dateRules" dense)
-          template(v-slot:append)
-            q-icon(name="event" class="cursor-pointer")
-              q-popup-proxy(ref="qDateProxy" transition-show="scale" transition-hide="scale")
-                q-date(v-model="date" minimal
-                       :mask="$t('dateOutputMask')"
-                       :options="dateOptions"
-                       @input="() => $refs.qDateProxy.hide()")
+        the-date-picker-input(v-model="date")
 
-        q-input(filled v-model="name"
-                label="Your name *"
-                hint="Name and surname"
-                lazy-rules
-                :rules="[ val => val && val.length > 0 || 'Please type something']")
+        q-list(separator dense bordered padding class="rounded-borders")
+          q-item-label(header) Accounts
 
-        q-input(filled type="number"
-                v-model="age"
-                label="Your age *"
-                lazy-rules
-                :rules=`[val => val !== null && val !== '' || 'Please type your age',
-                         val => val > 0 && val < 100 || 'Please type a real age']`)
+          q-scroll-area(:thumb-style="thumbStyle"
+                        :bar-style="barStyle"
+                        style="height: 176px")
 
-        q-toggle(v-model="accept" label="I accept the license and terms")
+            q-item(tag="label" dense
+                   v-ripple :key="index"
+                   v-for="(account, index) in accounts")
 
-        div
-          q-btn(:label="$q.lang.label.create" type="submit" color="primary")
-          q-btn(label="Reset" type="reset" color="primary" flat class="q-ml-sm")
+              q-item-section(side top)
+                q-checkbox(v-model="checkedAccounts[account.name]")
+              q-item-section
+                q-item-label(class="ellipsis") {{ account.name }}
+              q-item-section(v-if="checkedAccounts[account.name]")
+                q-item-label sum
 </template>
 
 <script>
-import { date } from 'quasar'
+import TheDatePickerInput from '../components/TheDatePickerInput'
 
 export default {
+  components: {
+    TheDatePickerInput
+  },
   data: () => ({
     name: null,
     age: null,
-    currentDate: new Date(),
-    stringDate: date.formatDate(new Date()),
-    accept: false
-  }),
-  computed: {
-    date: {
-      get () {
-        return date.formatDate(this.currentDate, this.$t('dateOutputMask'))
+    date: '',
+    checkedAccounts: {},
+    accept: false,
+    thumbStyle: {
+      right: '4px',
+      borderRadius: '5px',
+      backgroundColor: '#027be3',
+      width: '5px',
+      opacity: 0.75
+    },
+    barStyle: {
+      right: '2px',
+      borderRadius: '9px',
+      backgroundColor: '#027be3',
+      width: '9px',
+      opacity: 0.2
+    },
+    accounts: [
+      {
+        name: 'Cash',
+        balance: 1000.00,
+        visible: true
       },
-      set (newDate) {
-        this.stringDate = newDate
-        this.currentDate = date.extractDate(newDate, this.$t('dateOutputMask'))
+      {
+        name: 'Credit',
+        balance: -3000.00,
+        visible: true
+      },
+      {
+        name: 'UkrSib',
+        balance: 5000.00,
+        visible: true
+      },
+      {
+        name: 'Privat',
+        balance: 0.00,
+        visible: true
       }
-    },
-    parseDate () {
-      return date => {
-        let day = '', month = '', year = ''
-        const mask = this.$t('dateOutputMask')
-        mask.split('').forEach((char, index) => {
-          switch (char) {
-            case 'D': day += date[index]
-              break
-            case 'M': month += date[index]
-              break
-            case 'Y': year += date[index]
-              break
-          }
-        })
-        return { day, month, year }
-      }
-    },
-    dateRules () {
-      const parsedDate = this.parseDate(this.stringDate)
-      let { day, month, year } = parsedDate
-      console.log('parsedDate', parsedDate)
-      const maxYear = new Date().getFullYear() + 1
-      const minYear = new Date().getFullYear() - 1
-      const daysInMonth = new Date(year, month, 0).getDate()
-      return [
-        val => (year <= maxYear && year > minYear) || this.$t('dateInvalidYear'),
-        val => (month <= 12 && month > 0) || this.$t('dateInvalidMonth'),
-        val => (day <= daysInMonth && daysInMonth > 0) || this.$t('dateInvalidDay')
-      ]
-    },
-    dateOptions () {
-      const startDate = date.subtractFromDate(new Date(), { year: 1 })
-      const endDate = date.addToDate(new Date(), { year: 1 })
-      const dateF = unformedDate => date.formatDate(unformedDate, 'YYYY/MM/DD')
-      return date => date >= dateF(startDate) && date <= dateF(endDate)
-    }
-  },
+    ]
+  }),
   methods: {
     onSubmit () {
       if (this.accept !== true) {
